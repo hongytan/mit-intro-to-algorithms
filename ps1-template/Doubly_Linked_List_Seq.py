3bb34510bc7d002a2822f077813bd0a29c85cf58
hongytan <40774356+hongytan@users.noreply.github.com>
2023-05-04 23:19:35 -0700

    Filled in all todos, fails all 5 tests

--- ps1-template/Doubly_Linked_List_Seq.py
+++ ps1-template/Doubly_Linked_List_Seq.py
@@ -39,19 +39,32 @@ class Doubly_Linked_List_Seq:
         ###########################
         # Part (a): Implement me! #
         ###########################
-        pass
+        node = Doubly_Linked_List_Node(x)
+        if self.head:
+            node.next = self.head
+            self.head.prev = node
+        self.head = node
 
     def insert_last(self, x):
         ###########################
         # Part (a): Implement me! #
         ###########################
-        pass
+        node = Doubly_Linked_List_Node(x)
+        if self.tail:
+            node.prev = self.tail
+            self.tail.next = node
+        self.tail = node
 
     def delete_first(self):
         x = None
         ###########################
         # Part (a): Implement me! #
         ###########################
+        if self.head:
+            x = self.head.item
+            self.head = self.head.next
+        if self.head:
+            self.head.prev = None
         return x
 
     def delete_last(self):
@@ -59,6 +72,11 @@ class Doubly_Linked_List_Seq:
         ###########################
         # Part (a): Implement me! #
         ###########################
+        if self.tail:
+            x = self.tail.item
+            self.tail = self.tail.prev
+        if self.tail:
+            self.tail.next = None
         return x
 
     def remove(self, x1, x2):
@@ -66,10 +84,26 @@ class Doubly_Linked_List_Seq:
         ###########################
         # Part (b): Implement me! # 
         ###########################
+        x1.prev.next = x2.next
+        x2.next.prev = x1.prev
+        L2.head = x1
+        L2.tail = x2
+        x1.prev = None
+        x2.next = None
         return L2
 
     def splice(self, x, L2):
         ###########################
         # Part (c): Implement me! # 
         ###########################
-        pass
+        if x.next:
+            x.next.prev = L2.tail
+
+        L2.tail.next = x.next
+        
+        if L2.head:
+            L2.head.prev = x
+            
+        x.next = L2.head
+        L2.head = None
+        L2.tail = None
